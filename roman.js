cd0a39de5ce493bcf1bfe404253125111f780119
Jayr <jayralencarpereira@gmail.com>
2016-02-19 10:14:41 -0300

    Initial

---
+++ roman.js
@@ -0,0 +1,44 @@
+function convert(x){
+
+	var unitario = ['0','I','II','III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
+	var decimail = ['0','X', 'XX', 'XXX', 'XL', 'L', 'LX','LXX', 'LXXX','XC'];
+	var centena  = ['0','C','CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM'];
+	var milhar   = ['0','M', 'MM', 'MMM'];
+
+	x = parseInt(x).toString();
+	if(parseInt(x)==0){
+		return '';
+	}
+
+	//unidades
+	if(parseInt(x)<10){
+		value = parseInt(x);
+		return (value!=0) ? unitario[value] : '';
+	}
+	//decimais
+	if(parseInt(x)<100){
+		var parts = x.split('');
+		var numero = parseInt(parts[0]);
+		parts.splice(0,1)
+		return decimail[parseInt(numero)] + convert(parts.join(''));
+	}
+	//centenas
+	if(parseInt(x)<1000){
+		var parts = x.split('');
+		var numero = parseInt(parts[0]);
+		parts.splice(0,1)
+		return centena[parseInt(numero)] + convert(parts.join(''));
+
+	}
+	
+	//milhar
+	if(parseInt(x)<4000){
+		var parts = x.split('');
+		var numero = parseInt(parts[0]);
+		parts.splice(0,1)
+		return milhar[parseInt(numero)] + convert(parts.join(''));
+	}else{
+		return 'Não é possível converter';
+	}
+}
+
